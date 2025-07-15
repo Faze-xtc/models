8e9c39277e3eaa0e857f4384d602dba2c3fbb796
Faze-xtc <Offical.Faze360@gmail.com>
2025-07-15 14:12:02 -0400

    Delete DeerValues.js

--- DeerValues.js
+++
@@ -1,9 +0,0 @@
-// deerValues.js
-// This file exports constant initial values for deer attributes as an ES Module.
-
-export const DEER_INITIAL_HEALTH = 100;
-export const DEER_INITIAL_HUNGER = 80;
-export const DEER_INITIAL_THIRST = 80;
-export const DEER_INITIAL_ENERGY = 100;
-export const DEER_WALK_SPEED = 5;
-export const DEER_RUN_SPEED = 10;

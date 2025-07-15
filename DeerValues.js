03737d0c462aa39875a3093dea72480634db2302
Faze-xtc <Offical.Faze360@gmail.com>
2025-07-15 14:04:11 -0400

    DeerValues.js

--- DeerValues.js
+++ DeerValues.js
@@ -1,9 +1,10 @@
 // deerValues.js
-// This file defines constant initial values for deer attributes as global variables.
+// This file defines constant initial values for deer attributes as global variables,
+// explicitly attaching them to the window object for accessibility in module scripts.
 
-const DEER_INITIAL_HEALTH = 100;
-const DEER_INITIAL_HUNGER = 80;
-const DEER_INITIAL_THIRST = 80;
-const DEER_INITIAL_ENERGY = 100;
-const DEER_WALK_SPEED = 5;
-const DEER_RUN_SPEED = 10;
+window.DEER_INITIAL_HEALTH = 100;
+window.DEER_INITIAL_HUNGER = 80;
+window.DEER_INITIAL_THIRST = 80;
+window.DEER_INITIAL_ENERGY = 100;
+window.DEER_WALK_SPEED = 5;
+window.DEER_RUN_SPEED = 10;

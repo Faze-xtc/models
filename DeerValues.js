4f0d4084509d40766b3c0127655e4b3e94963a9b
Faze-xtc <Offical.Faze360@gmail.com>
2025-07-15 13:03:20 -0400

    DeerValues.js
    this file contains the deers stats

---
+++ DeerValues.js
@@ -0,0 +1,9 @@
+// deerValues.js
+// This file exports constant initial values for deer attributes.
+
+export const DEER_INITIAL_HEALTH = 100;
+export const DEER_INITIAL_HUNGER = 80;
+export const DEER_INITIAL_THIRST = 80;
+export const DEER_INITIAL_ENERGY = 100;
+export const DEER_WALK_SPEED = 5;
+export const DEER_RUN_SPEED = 10;
